d6bdf988bf8d5cad5c4578aa50a1a442f0461bfb
deborahlim <deborahlimhy@gmail.com>
2022-04-25 22:52:29 +0800

    handle async errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,26 @@
 const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+// catch async errors
+// to get rid of try catch blocks wrapped async function inside of catchAsync
+// catchAsync will return an anonymous function which will be assigned to get all users
+// when a request is made to the get all users handler, the anonymous functon will run
+// which returns the inner function which is the async function which returns a promise. If the promise gets rejected, we can catch the error
+// with the catch method available on all promises
+// the catch method will pass the error into the next function, and the error will end up in the global handling middleware
 
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    console.log("ALL USERS", users);
-    // send response
-    res.status(200).json({
-      status: "success",
-      results: users.length,
-      data: {
-        users: users,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e,
-    });
-  }
-};
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // send response
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users: users,
+    },
+  });
+});
 
-exports.createUser = async (req, res) => {
-  try {
+exports.createUser = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
 
     res.status(201).json({
@@ -30,16 +29,9 @@ exports.createUser = async (req, res) => {
         user: newUser,
       },
     });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e,
-    });
-  }
-};
+});
 
-exports.findUserById = async (req, res) => {
-  try {
+exports.findUserById = catchAsync(async (req, res, next) => {
     let user = await User.findById(req.params.id);
 
     res.status(200).json({
@@ -48,16 +40,9 @@ exports.findUserById = async (req, res) => {
         user: user,
       },
     });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e,
-    });
-  }
-};
+  });
 
-exports.updateUserById = async (req, res) => {
-  try {
+exports.updateUserById = catchAsync(async (req, res, next) => {
     let updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true, // true to return the modified document rather than the original. defaults to false
       runValidators: true, // if true runs update validators which validate the update operation against the model's schema
@@ -69,27 +54,14 @@ exports.updateUserById = async (req, res) => {
         user: updatedUser,
       },
     });
-  } catch (e) {
-    res.status(400).json({
-      status: "fail",
-      message: e,
-    });
-  }
-};
+});
 
-exports.deleteUserById = async (req, res) => {
-  try {
+exports.deleteUserById = catchAsync(async (req, res, next) => {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: "success",
       data: deletedUser,
     });
-  } catch (e) {
-    res.status(404).json({
-      status: "fail",
-      message: e,
-    });
-  }
-};
+});
 
 // Model.prototype = methods available on all the instances created through a model not the model itself

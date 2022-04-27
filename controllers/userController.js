7e62f83ca9665a0622644b27e2acb213bcbde9ba
deborahlim <deborahlimhy@gmail.com>
2022-04-27 19:29:07 +0800

    handle 404 not found errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
-// catch async errors
-// to get rid of try catch blocks wrapped async function inside of catchAsync
+const AppError = require("../utils/appError");
+// catch async errors with try catch blocks is not ideal
+// alternatively, can wrap async function inside of a wrapper function
 // catchAsync will return an anonymous function which will be assigned to get all users
 // when a request is made to the get all users handler, the anonymous functon will run
 // which returns the inner function which is the async function which returns a promise. If the promise gets rejected, we can catch the error
@@ -21,47 +22,58 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.createUser = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+  const newUser = await User.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        user: newUser,
-      },
-    });
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
 });
 
 exports.findUserById = catchAsync(async (req, res, next) => {
-    let user = await User.findById(req.params.id);
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        user: user,
-      },
-    });
+  let user = await User.findById(req.params.id);
+  if (!user) {
+    // need to put return keyword here so that we do not move on to the next line and send two responses
+    // go straight to global error handling middleware
+    return next(new AppError("No user found with that ID", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: user,
+    },
   });
+});
 
 exports.updateUserById = catchAsync(async (req, res, next) => {
-    let updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, // true to return the modified document rather than the original. defaults to false
-      runValidators: true, // if true runs update validators which validate the update operation against the model's schema
-    });
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        user: updatedUser,
-      },
-    });
+  let updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // true to return the modified document rather than the original. defaults to false
+    runValidators: true, // if true runs update validators which validate the update operation against the model's schema
+  });
+
+  if(!updatedUser) {
+    return next(new AppError("No user found by that ID", 404))
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
 });
 
 exports.deleteUserById = catchAsync(async (req, res, next) => {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: deletedUser,
-    });
+  const deletedUser = await User.findByIdAndDelete(req.params.id);
+  if(!deletedUser) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+  res.status(204).json({
+    status: "success",
+    data: deletedUser,
+  });
 });
 
 // Model.prototype = methods available on all the instances created through a model not the model itself
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,6 @@
 module.exports = (fn) => {
     return (req, res, next) => {
+      // next will automatically be called with the err parameter
       fn(req, res, next).catch(next);
     };
   };

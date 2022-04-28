907e64664934f7d811e2daa61b2b493ebcb50f0b
deborahlim <deborahlimhy@gmail.com>
2022-04-29 01:18:09 +0800

    wait for server to close before shutting down app

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const dotenv = require("dotenv");
 const { download } = require("express/lib/response");
 const mongoose = require("mongoose");
 
+// handle synchronous code; uncaught exceptions
+// should be at the top before any code executes
+// uncaught exceptions will have nothing to do with the server, 
+// so dont need to wait for server to close before shutting down application
+
 process.on("uncaughtException", () => {
   console.log("UNCAUGHT EXCEPTION SHUTTING DOWN...");
   console.log(err.name, err.message);
@@ -25,15 +30,25 @@ mongoose.connect(DB).then((con) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
+// handle asynchronous code
+// unhandled promise rejection; somewhere there was a promise that was rejected but was not handled
+// subscribe to the unhandledRejection event that will be emitted by the process object when there is a unhandled promise rejection
+// central place to handle all unhandled promised rejection as a last safety net
 process.on("unhandledRejection", () => {
   console.log("UNHANDLED REJECTION! SHUTTING DOWN...");
   console.log(err.name, err.message);
+  // shut down gracefully by closing the server first before shutting down app
+  // process.exit() will immediately abort all the requests that are currently still running or pending
+  // gives the server time to finish all the requests that are pending or being handled
   server.close(() => {
+    // to shut down application
+    // 0 stands for success
+    // 1 stands for uncaught exception
     process.exit(1);
+    // will also restart application in real world
   })
 });
 

db514c36492c0a8a1daee69f35775e6f6ea6d66e
deborahlim <deborahlimhy@gmail.com>
2022-04-29 13:09:47 +0800

    manage passwords, validate user password input

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 // const crypto = require("crypto");
 const mongoose = require("mongoose");
 const validator = require("validator");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,7 +28,34 @@ const userSchema = new mongoose.Schema({
       minlength: 8,
       select: false
   },
+  passwordConfirm: {
+    type: String,
+    // required a an input but not required to be persisted in the database
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function(el) {
+        return el === this.password
+      }
+    }
+  }
 });
+
+userSchema.pre("save", async function(next) {
+  // isModified methods is available on mongoose documents
+  // only run this function if password was actually modified
+  if(!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordCOnfirm field
+  this.passwordConfirm = undefined;
+  next();
+})
+
+
+
+
 
 const User = mongoose.model("User", userSchema);
 

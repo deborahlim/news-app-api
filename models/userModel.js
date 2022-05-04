3eb1119d6ef759136e0bcfd4a400b092f6f9f6c2
deborahlim <deborahlimhy@gmail.com>
2022-05-04 16:33:47 +0800

    add middleware to protect tour routes

--- models/userModel.js
+++ models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please tell us your email"],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"]
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   photo: String,
   role: {
@@ -23,30 +23,31 @@ const userSchema = new mongoose.Schema({
     default: "user",
   },
   password: {
-      type: String,
-      required: [true, "Please provide a password"],
-      minlength: 8,
-      // sets default select() behavior for this path, set to true if this path should always be selected
-      // set to false if it should be excluded by default
-      // can be overridden at the query level
-      select: false
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    // sets default select() behavior for this path, set to true if this path should always be selected
+    // set to false if it should be excluded by default
+    // can be overridden at the query level
+    select: false,
   },
   passwordConfirm: {
     type: String,
     // required a an input but not required to be persisted in the database
     required: [true, "Please confirm your password"],
     validate: {
-      validator: function(el) {
-        return el === this.password
-      }
-    }
-  }
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+  },
+  passwordChangedAt: Date,
 });
 
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function (next) {
   // isModified methods is available on mongoose documents
   // only run this function if password was actually modified
-  if(!this.isModified("password")) return next();
+  if (!this.isModified("password")) return next();
 
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
@@ -54,21 +55,38 @@ userSchema.pre("save", async function(next) {
   // Delete passwordCOnfirm field
   this.passwordConfirm = undefined;
   next();
-})
+});
 
 // instance method is available on all documents of a certain collection
 // this points to the current document; need to pass in the hashedPassword too bcos password is not available on the document
-userSchema.methods.correctPassword = async function(candidatePassword, hashedUserPassword) {
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  hashedUserPassword
+) {
   // candidatePassword = password that the user passes to req.body
   // userPassword = Data to be compared to
   // returns promise if callback is not specified
   // return true if password is the same and false if not
   return await bcrypt.compare(candidatePassword, hashedUserPassword);
-}
+};
 
-
-
-
+// JWTTimeStamp is the timestamp that the token was issued
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    console.log(changedTimeStamp)
+    // WE DO NOT WANT TO GIVE ACCESS TO THE PROTECTED ROUTE WHEN THE TOKEN IS ISSUED BEFORE THE PASSWORD IS CHANGED
+    // PASSWORD CHANGED TIMESTAMP SHOULD ALWAYS BE LESSER THAN TOKEN ISSUED TIMESTAMP
+    // We WANT TO RETURN FALSE HERE
+    console.log(JWTTimestamp < changedTimeStamp)
+    return JWTTimestamp < changedTimeStamp;
+  }
+  // false means password not changed
+  return false;
+};
 
 const User = mongoose.model("User", userSchema);
 
